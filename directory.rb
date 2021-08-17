2e9d2996ba99b587fef2de6890da75f0456da478
EC2 Default User <ec2-user@ip-172-31-45-169.us-east-2.compute.internal>
2021-08-17 11:24:50 +0000

    Adding a total number of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First we print out a list of the students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Then we print out the total number of students 
+print "Overall, we have "
+print 11
+puts " great students"
